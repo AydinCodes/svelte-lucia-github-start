379bf0dde8c8edfa187270a94fb3aee3f98ee409
Aydin Elhan <aydinmate@protonmail.com>
2024-04-13 17:16:12 +1000

    remove authentication-card

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,5 +1,18 @@
-<script>
-	import AuthenticationCard from "$lib/components/authentication-card.svelte";
+<script lang="ts">
+	import Github from '$lib/icons/github.svelte';
 
 </script>
-<AuthenticationCard href="/"/>
+
+<div class="my-auto flex h-[80vh] items-center justify-center">
+	<div class="flex flex-col space-y-6 p-12">
+		<p class="text-2xl">Log in to Your Account</p>
+		<div class="grid grid-cols-1 gap-6">
+			<a href="/">
+				<button class="w-full btn variant-filled-primary rounded">
+					<Github class="mr-2 h-4 w-4" />
+					GitHub
+				</button></a
+			>
+		</div>
+	</div>
+</div>
